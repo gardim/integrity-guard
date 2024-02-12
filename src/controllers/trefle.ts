5b2cb53c8d84a7d2960436e2e8913feafb3dd1f2
Fernando Pimenta <fernando.s.pimenta18@gmail.com>
2024-02-12 16:47:52 -0300

    Add prettier

--- src/controllers/trefle.ts
+++ src/controllers/trefle.ts
@@ -1,60 +1,59 @@
-import axios from "axios";
-import express from "express";
-import { Application } from "express";
-import createApp from "../config";
+import axios from 'axios';
+import express from 'express';
+import { Application } from 'express';
+import createApp from '../config';
 
 const app: Application = createApp();
 
-const trefleApiUrl: string = app.get("trefleApiUrl") as string;
-const trefleApiKey: string = app.get("trefleApiKey") as string;
+const trefleApiUrl: string = app.get('trefleApiUrl') as string;
+const trefleApiKey: string = app.get('trefleApiKey') as string;
 
 class TrefleController {
+	async get(req: express.Request, res: express.Response): Promise<void> {
+		const id: string = req.query.id as string;
 
-  async get(req: express.Request, res: express.Response): Promise<void> {
-    const id: string = req.query.id as string;
+		try {
+			const response = await axios.get(`${trefleApiUrl}/species/${id}`, {
+				params: {
+					token: trefleApiKey,
+				},
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
 
-    try {
-      const response = await axios.get(`${trefleApiUrl}/species/${id}`, {
-        params: {
-          token: trefleApiKey,
-        },
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+			console.log(response.data);
+			res.send(response.data);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Internal Server Error');
+		}
+	}
 
-      console.log(response.data);
-      res.send(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  }
+	async post(req: express.Request, res: express.Response): Promise<void> {
+		const query: string[] = req.body;
+		const encodedQuery: string = query.join(' ');
 
-  async post(req: express.Request, res: express.Response): Promise<void> {
-    const query: string[] = req.body;
-    const encodedQuery: string = query.join(' ');
+		console.log(encodedQuery);
 
-    console.log(encodedQuery);
+		try {
+			const response = await axios.get(`${trefleApiUrl}/plants/search`, {
+				params: {
+					token: trefleApiKey,
+					q: encodedQuery,
+				},
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
 
-    try {
-      const response = await axios.get(`${trefleApiUrl}/plants/search`, {
-        params: {
-          token: trefleApiKey,
-          'q': encodedQuery,
-        },
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      console.log(response.data);
-      res.send(response.data);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  }
+			console.log(response.data);
+			res.send(response.data);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Internal Server Error');
+		}
+	}
 }
 
 export default new TrefleController();

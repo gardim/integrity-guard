0782ace419d95e040545bf3b7fff137d4bf78097
Fernando Pimenta <fernando.s.pimenta18@gmail.com>
2024-02-12 15:47:29 -0300

    Remove netlify (#12)

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
 const app = require("./config")();
 const port = app.get("port");
-const serverless = require("serverless-http");
 
+app.use("/", require("./routes"));
 
-app.use(`/.netlify/functions/api`, require("./routes"));
-
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});

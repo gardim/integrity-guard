a45a029a20d0899f7edc6b23f04b263da5859313
Fernando Pimenta <fernando.s.pimenta18@gmail.com>
2024-02-12 20:38:18 -0300

    Add eslint

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,12 @@
 import createApp from './config/index';
 import { Application } from 'express';
+import routes from './routes';
+
 const app: Application = createApp();
 
 const port: number = app.get('port') as number;
 
-app.use('/', require('./routes').default);
+app.use('/', routes);
 
 app.listen(port, () => {
 	console.log(`Servidor rodando na porta ${port}`);

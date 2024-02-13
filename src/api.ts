0cc62508d91cab32c53fd62211b562105054573f
Fernando Pimenta <fernando.s.pimenta18@gmail.com>
2024-02-13 14:24:28 -0300

    Add swagger

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,14 @@
 import createApp from './config/index';
+import swaggerUi from 'swagger-ui-express';
 import { Application } from 'express';
 import routes from './routes';
+import swaggerSpec from '../swagger';
 
 const app: Application = createApp();
-
 const port: number = app.get('port') as number;
 
 app.use('/', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
 	console.log(`Servidor rodando na porta ${port}`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,119 @@ import express, { Response } from 'express';
 const routes = express.Router();
 
 import TrefleController from '../controllers/trefle';
-import WeatherstackController from '../controllers/weatherstack';
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Plant
+ *   - name: Others
+ */
 
 routes.get('/', (req, res): Response => {
 	return res.send('It works! 🚀');
 });
 
-routes.get('/trefle', TrefleController.get);
-routes.post('/trefle', TrefleController.post);
-routes.post('/weather', WeatherstackController.post);
+/**
+ * @swagger
+ * /plant/{name}:
+ *   get:
+ *     summary: Get specific species information
+ *     description: Returns information about a specific species.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Species information.
+ *       500:
+ *         description: Internal Server Error.
+ *     tags:
+ *       - Plant
+ */
+routes.get('/plant/{name}', TrefleController.get);
+
+/**
+ * @swagger
+ * /search/text:
+ *   post:
+ *     summary: Get a list of species by text
+ *     description: Returns a list of species based on query keywords.
+ *     parameters:
+ *       - in: query
+ *         name: q
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of species.
+ *       500:
+ *         description: Internal Server Error.
+ *     tags:
+ *       - Plant
+ */
+routes.post('/search/text', TrefleController.post);
+
+/**
+ * @swagger
+ * /query/image:
+ *   post:
+ *     summary: Get a list of species by image
+ *     description: Returns a list of species based on query images.
+ *     parameters:
+ *       - in: query
+ *         name: q
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of species.
+ *       500:
+ *         description: Internal Server Error.
+ *     tags:
+ *       - Plant
+ */
+routes.post('/query/image', TrefleController.post);
+
+/**
+ * @swagger
+ * /translate:
+ *   post:
+ *     summary: Get a translation
+ *     description: Returns a list of species based on query images.
+ *     parameters:
+ *       - in: query
+ *         name: q
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of species.
+ *       500:
+ *         description: Internal Server Error.
+ *     tags:
+ *       - Others
+ */
+routes.post('/translate', TrefleController.post);
+
+/**
+ * @swagger
+ * /weather:
+ *   post:
+ *     summary: Send data to Weatherstack
+ *     description: Sends data to Weatherstack.
+ *     responses:
+ *       200:
+ *         description: Data sent successfully.
+ *       500:
+ *         description: Internal Server Error.
+ *     tags:
+ *       - Others
+ */
 
 export default routes;
